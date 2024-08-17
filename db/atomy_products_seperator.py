a8a2e42f47451eb8502752d931dcd78d74bcf760
Totoya888 <uhong0628@gmail.com>
2024-08-17 19:47:48 +0800

    Seperate Json File

--- db/atomy_products_seperator.py
+++ db/atomy_products_seperator.py
@@ -1,9 +1,9 @@
 import json
 
-with open('db/atomy_category.json') as f:
+with open('db/atomy_products.json') as f:
   data = json.load(f)
   print(data["date"])
-  for i, c in enumerate(data["category"]):
-    file = open(f'db/atomy_category/atomy_category_{i}.json', 'w', encoding='utf-8')
+  for i, c in enumerate(data["body"]):
+    file = open(f'db/atomy_products/atomy_products_{i}.json', 'w', encoding='utf-8')
     with_date_data = {"date": data["date"], "body": c}
     json.dump(with_date_data, file, indent=2, ensure_ascii=False)
